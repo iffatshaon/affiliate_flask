0d355f319dd4bf05e85eb960ba9d413ff0e075b8
Iffat Hossain <iffatshaon@gmail.com>
2023-11-20 18:47:38 +0600

    Added documentation Modified gitignore to ignore .env Removed .env

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask
 from Controller import create_blueprint
 import os
+from flask_swagger_ui import get_swaggerui_blueprint
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+
+# Swagger documentation
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Backend"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 @app.route("/")
 def main():

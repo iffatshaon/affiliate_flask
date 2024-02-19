b6047cd298286646bc98748a5d6e3127d109b446
Iffat Hossain <iffatshaon@gmail.com>
2024-02-19 02:10:39 -0500

    captcha sum mode

--- Model/captcha_model.py
+++ Model/captcha_model.py
@@ -5,6 +5,7 @@ from io import BytesIO,StringIO
 from captcha.image import ImageCaptcha
 import random
 import uuid
+from math import floor
 
 class captcha_model:
     def __init__(self):
@@ -15,12 +16,14 @@ class captcha_model:
         self.con.reconnect()
         captcha_length = 5
         captcha_characters = string.ascii_letters + string.digits
-        captcha_text = ''.join(random.choice(captcha_characters) for _ in range(captcha_length))
+        # captcha_text = ''.join(random.choice(captcha_characters) for _ in range(captcha_length))
+        fnumber = floor(random.random()*99)
+        lnumber = floor(random.random()*99)
+        captcha_text = str(fnumber)+"+"+str(lnumber)
         image = ImageCaptcha(width=250, height=90, fonts=['assets/fonts/roman.ttf'])
         captcha_image = image.generate(captcha_text)
-        print(type(captcha_image))
         hash_value = uuid.uuid4().hex
-        self.cur.execute(f"INSERT INTO captcha(hash,text) VALUES('{hash_value}','{captcha_text}')")
+        self.cur.execute(f"INSERT INTO captcha(hash,text) VALUES('{hash_value}','{str(fnumber+lnumber)}')")
         captcha_image = BytesIO(captcha_image.read())
         captcha_image.seek(0)
         headers = {"Content-disposition": "attachment; filename=captcha.png","hash":hash_value}

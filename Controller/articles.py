d95ab383e756e1e300e0bc1ae14b90d5bef1ff81
M Iffat Hossain <iffatshaon@gmail.com>
2024-03-27 13:12:40 -0400

    Updated articles path

--- Controller/articles.py
+++ Controller/articles.py
@@ -1,34 +1,34 @@
 from flask import Blueprint, request
-from Model.article_model import article_model
+from Model.articles_model import articles_model
 
-model = article_model()
+model = articles_model()
 
-article = Blueprint("article_blueprint",__name__)
+articles = Blueprint("articles_blueprint",__name__)
 
-@article.route("/", methods=["POST"])
+@articles.route("/", methods=["POST"])
 def create():
     return model.create_model(request.get_json(), request.headers.get("Authorization"))
 
-@article.route("/createfree", methods=["POST"])
+@articles.route("/createfree", methods=["POST"])
 def createfree():
     return model.free_model(request.get_json())
 
-@article.route("/suggestion", methods=["POST"])
+@articles.route("/suggestion", methods=["POST"])
 def suggestion():
     return model.suggestion_model(request.get_json())
 
-@article.route("/", methods=["GET"])
+@articles.route("/", methods=["GET"])
 def get_list():
     return model.get_list_model(request.headers.get("Authorization"))
 
-@article.route("/keyword", methods=["POST"])
+@articles.route("/keyword", methods=["POST"])
 def keyword():
     return model.keyword_model(request.get_json())
 
-@article.route("/<id>", methods=["GET"])
+@articles.route("/<id>", methods=["GET"])
 def edit(id):
     return model.edit_model(id, request.headers.get("Authorization"))
 
-@article.route("/<id>", methods=["PUT"])
+@articles.route("/<id>", methods=["PUT"])
 def save(id):
     return model.save_model(id, request.get_json(), request.headers.get("Authorization"))
